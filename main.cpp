1adc36496385d661d68d6a14b67ecd895f3be432
Jonah Matteson <main@insberr.com>
2024-05-19 13:56:52 -0700

    Fullscreen rect, and using indexed drawing. Also passing a uniform for time and resolution

--- main.cpp
+++ main.cpp
@@ -51,6 +51,10 @@ GLuint compileShaders() {
     return shaderProgram;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main() {
     // Initialize GLFW
     if (!glfwInit()) {
@@ -61,8 +65,14 @@ int main() {
     // Set error callback
     glfwSetErrorCallback(error_callback);
 
+    // I am not sure what these do tbh
+    // todo figure out what these do
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Ray Marching Time!", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -71,6 +81,8 @@ int main() {
 
     // Make the window's context current
     glfwMakeContextCurrent(window);
+    // todo figure out what this does too
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Initialize GLEW
     GLenum err = glewInit();
@@ -82,22 +94,32 @@ int main() {
     GLuint shaderProgram = compileShaders();
 
     // Set up vertex data and buffers and configure vertex attributes
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+    float quadVertices[] = {
+        -1.0f, -1.0f,
+         1.0f, -1.0f,
+         1.0f,  1.0f,
+        -1.0f,  1.0f
     };
 
-    GLuint VBO, VAO;
+    GLuint indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -108,10 +130,16 @@ int main() {
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw the triangle
+        // Set the shaders
         glUseProgram(shaderProgram);
+
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glUniform2f(glGetUniformLocation(shaderProgram, "Resolution"), width, height);
+        glUniform1f(glGetUniformLocation(shaderProgram, "Time"), glfwGetTime());
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -121,7 +149,12 @@ int main() {
     }
 
     // Clean up and exit
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
     glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
